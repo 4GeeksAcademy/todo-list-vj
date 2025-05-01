5ac6d45c504d5c09d799e597ff77269798a95e6b
Vicente Jiménez <vicentejimenez.actor@gmail.com>
2025-05-01 01:15:10 +0000

    casi terminado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,61 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [todoList, setTodoList] = useState([])
+	const [newTodo, setNewTodo] = useState("")
+	const [showX, setShowX] = useState(null)
+
+	const handlePressKey = (e) => {
+		if(e.key === "Enter") {
+			setTodoList([...todoList, newTodo])
+			setNewTodo("")
+		}
+	}
+
+	const handleDelete = (indexToDelete) => {
+		
+		setTodoList(todoList.filter((elem, index) => index !== indexToDelete))
+	}
+
+
+
 	return (
 		<div className="text-center">
             
+			<div>List of todos:</div>
+			<div id="contenedor-todo-list">
+				<ul>
+					<li>
+						<input 
+						type="text" 
+						placeholder="What needs to be done?" 
+						value={newTodo}
+						onChange={(e)=> setNewTodo(e.target.value)}
+						onKeyDown={handlePressKey}
+						/>
+						
+					</li>
+					{
+						todoList.map((todo, index)=>(
+							<li key={index}
+							onMouseOver={() => setShowX(index)}
+							onMouseLeave={() => setShowX(null)}
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+							>{todo}
+							{showX === index && <small className="mx-5" onClick={() => handleDelete(index)}>X</small>}
+							</li>
+						))
+					}
+
+					<li>{todoList.length === 0 ? "There are no things to do, add one." : " Things to do: " + todoList.length}</li>		
+				</ul>
+			</div>
+
 		</div>
 	);
 };
