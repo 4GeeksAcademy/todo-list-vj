0e0dfdd5b8910e3db760f84c4096715350dfa6da
Vicente Jiménez <vicentejimenez.actor@gmail.com>
2025-05-02 02:18:36 +0000

    proyecto final

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,9 +1,6 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
 
 	const [todoList, setTodoList] = useState([])
@@ -11,51 +8,54 @@ const Home = () => {
 	const [showX, setShowX] = useState(null)
 
 	const handlePressKey = (e) => {
-		if(e.key === "Enter") {
+		if (e.key === "Enter") {
 			setTodoList([...todoList, newTodo])
 			setNewTodo("")
 		}
 	}
 
 	const handleDelete = (indexToDelete) => {
-		
+
 		setTodoList(todoList.filter((elem, index) => index !== indexToDelete))
 	}
 
 
 
 	return (
-		<div className="text-center">
-            
-			<div>List of todos:</div>
+		<div id="div">
+
+			<div className="title">List of things to do:</div>
 			<div id="contenedor-todo-list">
-				<ul>
+				<ul className="listado">
 					<li>
-						<input 
-						type="text" 
-						placeholder="What needs to be done?" 
-						value={newTodo}
-						onChange={(e)=> setNewTodo(e.target.value)}
-						onKeyDown={handlePressKey}
+						<input
+							type="text"
+							placeholder="What needs to be done?"
+							value={newTodo}
+							onChange={(e) => setNewTodo(e.target.value)}
+							onKeyDown={handlePressKey}
 						/>
-						
+
 					</li>
+					<div>
 					{
-						todoList.map((todo, index)=>(
+						todoList.map((todo, index) => (
 							<li key={index}
-							onMouseOver={() => setShowX(index)}
-							onMouseLeave={() => setShowX(null)}
+								onMouseOver={() => setShowX(index)}
+								onMouseLeave={() => setShowX(null)}
+								
 
 							>{todo}
-							{showX === index && <small className="mx-5" onClick={() => handleDelete(index)}>X</small>}
+								{showX === index && <small onClick={() => handleDelete(index)} id="equis"><i className="fa-solid fa-x"></i></small>}
 							</li>
 						))
 					}
-
-					<li>{todoList.length === 0 ? "There are no things to do, add one." : " Things to do: " + todoList.length}</li>		
+					</div>
+					<li className="things">{todoList.length === 0 ? "There are no things to do, add one." : " Things to do: " + todoList.length}</li>
 				</ul>
 			</div>
-
+			<div id="hoja-uno"></div>
+			<div id="hoja-dos"></div>
 		</div>
 	);
 };
